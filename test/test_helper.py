337ca946aa064e32d0afdc97f7b65dd527e1a5de
AlirezaDehlaghi <53132697+AlirezaDehlaghi@users.noreply.github.com>
2023-11-24 16:45:30 +0100

    Update test_helper.py

--- test/test_helper.py
+++ test/test_helper.py
@@ -6,3 +6,10 @@ from Helper import maximum
 
 def test_maximum():
     assert maximum([]) == 0
+
+def test_maximum():
+    assert maximum([2,3]) == 3
+
+
+def test_maximum():
+    assert maximum([2,3]) == 2
