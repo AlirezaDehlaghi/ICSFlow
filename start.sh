c4ceeacfce9ede400ac0384b1048fffae4cb2f87
alirezade <alirezade@WPPC1N9X16>
2024-01-11 00:31:25 +0100

    fix

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ printStep "Running ICSFlow with this arguments:"
 
 printStep ""
 
-sudo -E python3 src/ICSFlowGenerator.py sniff  --source br_icsnet --interval 0.5  --predictor input/ids.joblib --target_file   output/sniffed.csv --target_connection  input/sample_connection.txt
+sudo -E python3 src/ICSFlowGenerator.py sniff  --source br_icsnet --interval 0.5  --predictor input/ids.joblib --target_file   output/sniffed.csv --target_connection  input/connection.txt
 
 #printStep "running with these arguments sniff  --source br_ics_net --interval 0.5 --predictor  input/predict_model.joblib   --target_file   output/sniffed.csv --target_connection  sample_connection.txt"
 #python3 ICSFlowGenerator.py sniff  --source br_ics_net --interval 0.5 --predictor  input/predict_model.joblib   --target_file   output/sniffed.csv --target_connection  sample_connection.txt
