c4856961096c085bb4d3a3cc5a7fdbef1ecfaef0
alirezade <alirezade@WPPC1N9X16>
2024-01-22 18:39:04 +0100

    modify start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,10 @@ printStep(){
 printStep "Getting last changes pull request"
 sudo git pull
 
-printStep ("Check Requirement.txt")
+printStep "Check Requirement.txt"
 pip install -r requirements.txt
 
-printStep("Running ICSFlow with this arguments")
+printStep "Running ICSFlow with this arguments"
 
 source = "br_icsnet"
 interval = 0.5
@@ -32,7 +32,7 @@ echo "\t --predictor" predictor
 echo "\t --target_file" target_file
 echo "\t --target_connection" target_connection
 
-printStep ""
+printStep " "
 
 sudo -E python3 src/ICSFlowGenerator.py sniff  --source source --interval interval  --predictor predictor --target_file   target_file --target_connection  target_connection
 #sudo -E python3 src/ICSFlowGenerator.py sniff  --source br_icsnet --interval 0.5  --predictor input/ids.joblib --target_file   output/sniffed.csv --target_connection  input/connection.txt
