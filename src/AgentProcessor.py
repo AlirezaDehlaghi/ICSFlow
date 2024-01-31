38588dc05b8ec329feed21c424d23a5e06b203ad
alirezade <alirezade@WPPC1N9X16>
2024-01-31 10:48:48 +0100

    fix memory leak error

--- src/AgentProcessor.py
+++ src/AgentProcessor.py
@@ -11,13 +11,13 @@ class AgentProcessor:
         self.__processes = []
         annotator = ProcessAnnotator(predictor_address, attacks_address)
         # flow_file_writer = ProcessFlowFileWriter(file_address)
-        status_sender = ProcessStatusSenderMQTT(server_connection, voting_interval=Config.StatusSender.voting_interval)
+        # status_sender = ProcessStatusSenderMQTT(server_connection, voting_interval=Config.StatusSender.voting_interval)
         flow_sender = ProcessFlowSenderMQTT(server_connection)
 
         self.__processes.append(annotator)
         # self.__processes.append(flow_file_writer)
-        if annotator.is_prediction_enabled():
-            self.__processes.append(status_sender)
+        # if annotator.is_prediction_enabled():
+        #     self.__processes.append(status_sender)
         #self.__processes.append(flow_sender)
 
     def process(self, flow):
